033315005ffbeb5ab8606a5925471cfe561c82f1
jccamargo94 <camilo.camargo@guane.com.co>
2022-10-04 15:48:23 -0500

    add aerich migrations

--- project/app/main.py
+++ project/app/main.py
@@ -1,15 +1,30 @@
-#project/app/main.py
+# project/app/main.py
 
-from fastapi import Depends, FastAPI
+import os
+
+from fastapi import FastAPI, Depends
+from tortoise.contrib.fastapi import register_tortoise
 
 from app.config import get_settings, Settings
 
+
 app = FastAPI()
+
+
+register_tortoise(
+    app,
+    db_url=os.environ.get("DATABASE_URL"),
+    modules={"models": ["app.models.tortoise"]},
+    generate_schemas=False,  # updated
+    add_exception_handlers=True,
+)
+
 
 @app.get("/ping")
 async def pong(settings: Settings = Depends(get_settings)):
     return {
-        "ping": "pong",
+        "ping": "pong!",
         "environment": settings.environment,
-        "testing": settings.testing,
+        "testing": settings.testing
     }
+ 
