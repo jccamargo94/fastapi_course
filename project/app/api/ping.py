f9d0b0fa1e0cd8e7eba5fe67889ac8cc013f0c23
jccamargo94 <camilo.camargo@guane.com.co>
2022-10-04 16:03:25 -0500

    Add migrations with aerich

--- project/app/api/ping.py
+++ project/app/api/ping.py
@@ -1,10 +1,13 @@
 # project/app/api/ping.py
 
+
 from fastapi import APIRouter, Depends
 
 from app.config import get_settings, Settings
 
+
 router = APIRouter()
+
 
 @router.get("/ping")
 async def pong(settings: Settings = Depends(get_settings)):
