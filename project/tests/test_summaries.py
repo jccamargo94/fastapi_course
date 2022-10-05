4108977ec2cb0aa660103a97737eeb3f47309272
jccamargo94 <camilo.camargo@guane.com.co>
2022-10-05 16:22:00 -0500

    get all test

--- project/tests/test_summaries.py
+++ project/tests/test_summaries.py
@@ -16,7 +16,7 @@ def test_create_summary(test_app_with_db):
 
 def test_create_summaries_invalid_json(test_app):
     response = test_app.post("/summaries/", data=json.dumps({}))
-    assert response.status_code == 422
+    assert response.status_code == status.HTTP_422_UNPROCESSABLE_ENTITY
     assert response.json() == {
         "detail": [
             {
